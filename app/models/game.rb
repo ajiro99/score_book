3f876295f797e049ea3c4d15f56d4a73542df92d
ajiro <ajiro99@gmail.com>
2018-03-18 03:23:06 +0900

    modify fixtures

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,11 +1,11 @@
 class Game < ApplicationRecord
   extend Enumerize
-
+#TODO: pk追加
   belongs_to :convention
   belongs_to :opponent
   has_many :results
 
   enumerize :home_away, in: { home: 0, away: 1, neutral: 2 }, scope: true
   enumerize :weather, in: { sunny: 0, cloudy: 1, rain: 2, snow: 3 }, scope: true
-  enumerize :result, in: { win: 0, lose: 1, draw: 2 }, scope: true
+  enumerize :result, in: { win: 0, draw: 1, lose: 2, }, scope: true
 end
--- db/fixtures/player.rb
+++ db/fixtures/player.rb
@@ -1,4 +1,5 @@
 Player.seed do |s|
+  s.id = 1
   s.year = 2018
   s.number = 1
   s.name = "クォン　スンテ"
@@ -9,16 +10,7 @@ Player.seed do |s|
 end
 
 Player.seed do |s|
-  s.year = 2018
-  s.number = 21
-  s.name = "曽ヶ端　準"
-  s.position = 0
-  s.birthday = "1979/8/2"
-  s.height = 187
-  s.weight = 80
-end
-
-Player.seed do |s|
+  s.id = 2
   s.year = 2018
   s.number = 2
   s.name = "内田　篤人"
@@ -29,6 +21,7 @@ Player.seed do |s|
 end
 
 Player.seed do |s|
+  s.id = 3
   s.year = 2018
   s.number = 3
   s.name = "昌子　源"
@@ -39,6 +32,18 @@ Player.seed do |s|
 end
 
 Player.seed do |s|
+  s.id = 4
+  s.year = 2018
+  s.number = 4
+  s.name = "レオ　シルバ"
+  s.position = 2
+  s.birthday = "1985/12/24"
+  s.height = 181
+  s.weight = 76
+end
+
+Player.seed do |s|
+  s.id = 5
   s.year = 2018
   s.number = 5
   s.name = "植田　直通"
@@ -49,6 +54,106 @@ Player.seed do |s|
 end
 
 Player.seed do |s|
+  s.id = 6
+  s.year = 2018
+  s.number = 6
+  s.name = "永木 亮太"
+  s.position = 2
+  s.birthday = "1988/06/04"
+  s.height = 173
+  s.weight = 72
+end
+
+Player.seed do |s|
+  s.id = 7
+  s.year = 2018
+  s.number = 7
+  s.name = "ペドロ ジュニオール"
+  s.position = 3
+  s.birthday = "1987/01/29"
+  s.height = 182
+  s.weight = 75
+end
+
+Player.seed do |s|
+  s.id = 8
+  s.year = 2018
+  s.number = 8
+  s.name = "土居 聖真"
+  s.position = 2
+  s.birthday = "1992/05/21"
+  s.height = 172
+  s.weight = 63
+end
+
+Player.seed do |s|
+  s.id = 9
+  s.year = 2018
+  s.number = 9
+  s.name = "鈴木 優磨"
+  s.position = 3
+  s.birthday = "1996/04/26"
+  s.height = 182
+  s.weight = 75
+end
+
+Player.seed do |s|
+  s.id = 10
+  s.year = 2018
+  s.number = 10
+  s.name = "金崎 夢生"
+  s.position = 3
+  s.birthday = "1989/02/16"
+  s.height = 180
+  s.weight = 70
+end
+
+Player.seed do |s|
+  s.id = 11
+  s.year = 2018
+  s.number = 11
+  s.name = "レアンドロ"
+  s.position = 2
+  s.birthday = "1993/05/12"
+  s.height = 177
+  s.weight = 76
+end
+
+Player.seed do |s|
+  s.id = 12
+  s.year = 2018
+  s.number = 13
+  s.name = "中村 充孝"
+  s.position = 2
+  s.birthday = "1990/09/13"
+  s.height = 174
+  s.weight = 67
+end
+
+Player.seed do |s|
+  s.id = 13
+  s.year = 2018
+  s.number = 14
+  s.name = "金森 健志"
+  s.position = 3
+  s.birthday = "1994/04/04"
+  s.height = 171
+  s.weight = 74
+end
+
+Player.seed do |s|
+  s.id = 14
+  s.year = 2018
+  s.number = 15
+  s.name = "三竿 雄斗"
+  s.position = 1
+  s.birthday = "1991/04/16"
+  s.height = 175
+  s.weight = 70
+end
+
+Player.seed do |s|
+  s.id = 15
   s.year = 2018
   s.number = 16
   s.name = "山本　脩斗"
@@ -56,4 +161,180 @@ Player.seed do |s|
   s.birthday = "1985/6/1"
   s.height = 180
   s.weight = 69
+end
+
+Player.seed do |s|
+  s.id = 16
+  s.year = 2018
+  s.number = 19
+  s.name = "山口 一真"
+  s.position = 3
+  s.birthday = "1996/01/17"
+  s.height = 175
+  s.weight = 70
+end
+
+Player.seed do |s|
+  s.id = 17
+  s.year = 2018
+  s.number = 20
+  s.name = "三竿 健斗"
+  s.position = 2
+  s.birthday = "1996/04/16"
+  s.height = 181
+  s.weight = 73
+end
+
+Player.seed do |s|
+  s.id = 18
+  s.year = 2018
+  s.number = 21
+  s.name = "曽ヶ端　準"
+  s.position = 0
+  s.birthday = "1979/8/2"
+  s.height = 187
+  s.weight = 80
+end
+
+Player.seed do |s|
+  s.id = 19
+  s.year = 2018
+  s.number = 22
+  s.name = "西 大伍"
+  s.position = 1
+  s.birthday = "1987/08/28"
+  s.height = 176
+  s.weight = 73
+end
+
+Player.seed do |s|
+  s.id = 20
+  s.year = 2018
+  s.number = 23
+  s.name = "小田 逸稀"
+  s.position = 1
+  s.birthday = "1998/07/16"
+  s.height = 173
+  s.weight = 68
+end
+
+Player.seed do |s|
+  s.id = 21
+  s.year = 2018
+  s.number = 24
+  s.name = "伊東 幸敏"
+  s.position = 1
+  s.birthday = "1993/09/03"
+  s.height = 176
+  s.weight = 68
+end
+
+Player.seed do |s|
+  s.id = 22
+  s.year = 2018
+  s.number = 25
+  s.name = "遠藤 康"
+  s.position = 2
+  s.birthday = "1988/04/07"
+  s.height = 168
+  s.weight = 69
+end
+
+Player.seed do |s|
+  s.id = 23
+  s.year = 2018
+  s.number = 26
+  s.name = "久保田 和音"
+  s.position = 2
+  s.birthday = "1997/01/01"
+  s.height = 170
+  s.weight = 62
+end
+
+Player.seed do |s|
+  s.id = 24
+  s.year = 2018
+  s.number = 28
+  s.name = "町田 浩樹"
+  s.position = 1
+  s.birthday = "1997/08/25"
+  s.height = 190
+  s.weight = 80
+end
+
+Player.seed do |s|
+  s.id = 25
+  s.year = 2018
+  s.number = 29
+  s.name = "川俣 慎一郎"
+  s.position = 0
+  s.birthday = "1989/07/23"
+  s.height = 188
+  s.weight = 86
+end
+
+Player.seed do |s|
+  s.id = 26
+  s.year = 2018
+  s.number = 30
+  s.name = "安部 裕葵"
+  s.position = 3
+  s.birthday = "1999/01/28"
+  s.height = 171
+  s.weight = 65
+end
+
+Player.seed do |s|
+  s.id = 27
+  s.year = 2018
+  s.number = 31
+  s.name = "沖 悠哉"
+  s.position = 0
+  s.birthday = "1999/08/22"
+  s.height = 184
+  s.weight = 72
+end
+
+Player.seed do |s|
+  s.id = 28
+  s.year = 2018
+  s.number = 32
+  s.name = "安西 幸輝"
+  s.position = 1
+  s.birthday = "1995/05/31"
+  s.height = 172
+  s.weight = 64
+end
+
+Player.seed do |s|
+  s.id = 29
+  s.year = 2018
+  s.number = 36
+  s.name = "田中 稔也"
+  s.position = 2
+  s.birthday = "1997/12/02"
+  s.height = 175
+  s.weight = 65
+end
+
+Player.seed do |s|
+  s.id = 30
+  s.year = 2018
+  s.number = 39
+  s.name = "犬飼 智也"
+  s.position = 1
+  s.birthday = "1993/05/12"
+  s.height = 182
+  s.weight = 77
+end
+
+Player.seed do |s|
+  s.id = 31
+  s.year = 2018
+  s.number = 40
+  s.name = "小笠原 満男"
+  s.position = 2
+  s.birthday = "1979/04/05"
+  s.height = 173
+  s.weight = 72
 end
