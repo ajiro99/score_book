882fafab48fcca3083f280a9d4e60a2fc3c8c3ad
ajiro <ajiro99@gmail.com>
2018-03-19 02:55:15 +0900

    add modal to games

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,3 +15,21 @@
 //= require turbolinks
 //= require_tree .
 //= require materialize-sprockets
+
+$(function() {
+  $('tbody tr[data-href]').addClass('clickable').click( function() {
+    window.location = $(this).attr('data-href');
+  }).find('a').hover( function() {
+    $(this).parents('tr').unbind('click');
+  }, function() {
+    $(this).parents('tr').click( function() {
+      window.location = $(this).attr('data-href');
+    });
+  });
+
+  $('tr[rel*=leanModal]').leanModal({
+      top: 50,                     // モーダルウィンドウの縦位置を指定
+      overlay : 0.5              // 背面の透明度 
+
+  });
+}); 
