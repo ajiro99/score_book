532757bde20f7cd4882e1c6e2dfaf05279ed73a7
Yuichi Ajiro <ajiro99@gmail.com>
2018-04-13 20:57:24 +0900

    modify nave menue

---
+++ app/assets/javascripts/top.js
@@ -0,0 +1,41 @@
+$(function() {
+  var showFlag = false;
+  var topBtn = $('#page-top');
+  topBtn.css('bottom', '-100px');
+  var showFlag = false;
+  //スクロールが100に達したらボタン表示
+  $(window).scroll(function () {
+    if ($(this).scrollTop() > 100) {
+      if (showFlag == false) {
+        showFlag = true;
+        topBtn.stop().animate({'bottom' : '20px'}, 200);
+      }
+    } else {
+      if (showFlag) {
+        showFlag = false;
+        topBtn.stop().animate({'bottom' : '-100px'}, 200);
+      }
+    }
+  });
+  //スクロールしてトップ
+  topBtn.click(function () {
+    $('body,html').animate({
+      scrollTop: 0
+    }, 500);
+    return false;
+  });
+
+  var naviConfig = [
+    { rule:"/", selector:"#navi-root" },
+    { rule:"/about", selector:"#navi-about" }
+  ];
+
+  (function(config){
+    for(var i=0;i<config.length;i++){
+      if( location.pathname === config[i].rule ){
+        $(config[i].selector).addClass("active");
+        return;
+      }
+    }
+  })( naviConfig );
+});
