bb5f4bd1e37b119b22f9e7d9732c51d385300f8f
Yuichi Ajiro <ajiro99@gmail.com>
2018-04-18 15:43:09 +0900

    modify game detail modal

--- app/decorators/result_decorator.rb
+++ app/decorators/result_decorator.rb
@@ -2,8 +2,9 @@ class ResultDecorator < Draper::Decorator
   delegate_all
 
   def time_decorate
-    return h.raw("#{time}　#{h.fa_icon('angle-double-down lg')}") if participation.starting? && time < 90 
-    return h.raw("#{time}　#{h.fa_icon('angle-double-up lg')}") if participation.half_way_out? && time > 0 
+    return '' if time == 0
+    return h.raw("#{time}　#{h.fa_icon('angle-double-down lg')}") if participation.starting? && time < 90
+    return h.raw("#{time}　#{h.fa_icon('angle-double-up lg')}") if participation.half_way_out? && time > 0
     "#{time}　　"
   end
 end
