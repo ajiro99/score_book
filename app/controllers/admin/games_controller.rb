bd8ae226dc46fbebaedd04b9dc9c842d385b8df6
Yuichi Ajiro <ajiro99@gmail.com>
2018-04-24 00:46:45 +0900

    bug fix

--- app/controllers/admin/games_controller.rb
+++ app/controllers/admin/games_controller.rb
@@ -75,7 +75,7 @@ class Admin::GamesController < Admin::BaseController
         :result, :goal, :goal_against, :shoot, :shoot_against, :ck, :ck_against, :fk, :fk_against, :yellow_card,
         :gave_yellow_card, :red_card, :gave_red_card, :dominance_rate, :first_change_time, :first_change_out_id,
         :first_change_in_id, :second_change_time, :second_change_out_id, :second_change_in_id, :third_change_time,
-        :third_change_out_id, :third_change_in_id, :number_of_visitors, :rank
+        :third_change_out_id, :third_change_in_id, :number_of_visitors, :rank, :pk, :pk_against, :video_url
       )
     end
 end
--- app/models/concerns/goal_pattern_base.rb
+++ app/models/concerns/goal_pattern_base.rb
@@ -6,7 +6,7 @@ module GoalPatternBase
     enumerize :pattern, in:
       { ck: 1, fk: 2, pk: 3, throw_in: 4, gk: 5,
         counter: 10, short_counter: 11, slow_attack: 12,
-        individual:20, own_goal: 99
+        individual:20, miss:21, own_goal: 99
       }, scope: true
     enumerize :starting_area, in: { left: 1, center: 2, right: 3 }, scope: true
     enumerize :goal_area, in: { pa_in: 1, pa_out: 2 }, scope: true
